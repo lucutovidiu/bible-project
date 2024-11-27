6321c20504b733a89a1efde04926b16851f21a03
OvidiuLucut <ovidiu.lucut@jettytechnologies.com>
2024-11-27 12:58:31 +0000

    fix/Renew Site certificates

--- docker/nginx/cert/install_certbot.sh
+++ docker/nginx/cert/install_certbot.sh
@@ -9,19 +9,30 @@ print_colored() {
 
 brew services stop certbot
 
-certbot certonly --standalone -d "bible.prowebart.co.uk" | tee ./cert-details
-
-sudo cat /etc/letsencrypt/live/prowebart.co.uk/fullchain.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/cert/prowebart/fullchain.pem
-sudo cat /etc/letsencrypt/live/prowebart.co.uk/privkey.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/prowebart/privkey.pem
+sudo certbot certonly --standalone -d "prowebart.co.uk" -d "bible.prowebart.co.uk" -d "bible-be.prowebart.co.uk"
 
 
-sudo cat /etc/letsencrypt/live/bible.prowebart.co.uk/fullchain.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/bible-proweb/fullchain.pem
-sudo cat /etc/letsencrypt/live/bible.prowebart.co.uk/privkey.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/bible-proweb/privkey.pem
 
-sudo cat /etc/letsencrypt/live/bible-be.prowebart.co.uk/fullchain.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/bible-be-proweb/fullchain.pem
-sudo cat /etc/letsencrypt/live/bible-be.prowebart.co.uk/privkey.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/bible-be-proweb/privkey.pem
-
-
+sudo cat /etc/letsencrypt/live/bible-be.prowebart.co.uk/fullchain.pem | tee /Users/ovidiulucut/work/personal_repos/bible-project/docker/nginx/certificates/wild_cert/fullchain.pem
+sudo cat /etc/letsencrypt/live/bible-be.prowebart.co.uk/privkey.pem | tee /Users/ovidiulucut/work/personal_repos/bible-project/docker/nginx/certificates/wild_cert/privkey.pem
 
 Certificate is saved at: /etc/letsencrypt/live/bible-be.prowebart.co.uk/fullchain.pem
 Key is saved at:         /etc/letsencrypt/live/bible-be.prowebart.co.uk/privkey.pem
+# certbot certonly --standalone -d "bible.prowebart.co.uk" | tee ./cert-details
+
+# sudo cat /etc/letsencrypt/live/prowebart.co.uk/fullchain.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/cert/prowebart/fullchain.pem
+# sudo cat /etc/letsencrypt/live/prowebart.co.uk/privkey.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/prowebart/privkey.pem
+# /Users/ovidiulucut/work/personal_repos/bible-project/docker/nginx/certificates/wild_cert
+
+# sudo cat /etc/letsencrypt/live/bible.prowebart.co.uk/fullchain.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/bible-proweb/fullchain.pem
+# sudo cat /etc/letsencrypt/live/bible.prowebart.co.uk/privkey.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/bible-proweb/privkey.pem
+
+# sudo cat /etc/letsencrypt/live/bible-be.prowebart.co.uk/fullchain.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/bible-be-proweb/fullchain.pem
+# sudo cat /etc/letsencrypt/live/bible-be.prowebart.co.uk/privkey.pem | tee /Users/ovidiulucut/work/Documents/personal_repos/bible-project/docker/nginx/certificates/bible-be-proweb/privkey.pem
+
+
+
+# Certificate is saved at: /etc/letsencrypt/live/bible-be.prowebart.co.uk/fullchain.pem
+# Key is saved at:         /etc/letsencrypt/live/bible-be.prowebart.co.uk/privkey.pem
+
+
